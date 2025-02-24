5e0546e90c56c89b2f2845901bd661223e1eb6ab
1234Chinonso <chinonsotasieobi@hotmail.com>
2025-02-24 18:43:27 +0100

    Update abstract.py
    All this is abstract like this code:
    class USA():
        def capital(self):
            print("Washington, D.C. is the capital of USA.")
     
        def language(self):
            print("English is the primary language of USA.")
     
        def type(self):
            print("USA is a developed country.")
     
    # Object Creation
    obj_ind = India()
    obj_usa = USA()
    
    # Common Interface
    for country in (obj_ind, obj_usa):
        country.capital()
        country.language()
        country.type()

--- abstract.py
+++ abstract.py
@@ -11,3 +11,55 @@ class test_class(Absclass):
 test_obj = test_class()
 test_obj.task()
 test_obj.print(100)
+
+class Animal(ABC):
+    def move(self):
+        pass
+class Human(Animal):
+    def move(self):
+        print("I can walk and run.")
+class Snake(Animal):
+    def move(self):
+        print("I can crawl.")
+class Dog(Animal):
+    def move(self):
+        print("I can bark.")
+class Lion(Animal):
+    def move(self):
+        print("I can roar.")
+R = Human()
+R.move()
+
+K = Snake()
+K.move()
+
+R = Dog()
+R.move()
+
+K = Lion()
+K.move()
+
+class India():
+    def capital(self):
+        print("New Delhi is the capital of India.")
+    def language(self):
+        print("Hindi is the most widely spoken language of India.")
+    def type (self):
+        print("India is a developing country.")
+class USA():
+    def capital(self):
+        print("Washington, D.C. is the capital of USA.")
+ 
+    def language(self):
+        print("English is the primary language of USA.")
+ 
+    def type(self):
+        print("USA is a developed country.")
+ 
+obj_ind = India()
+obj_usa = USA()
+
+for country in (obj_ind, obj_usa):
+    country.capital()
+    country.language()
+    country.type()
